b29a19e210d2cc28cf0d57ff055ad421fa84f4fa
Douglas W Schultz <vxpx17@gmail.com>
2017-12-30 02:04:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,9 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray (array,newEle){
-  return array.unshift(newEle);}
+  return [...array,newEle];}
+  
+function destructivelyAddElementToBeginningOfArray(array,newEle){
+  return array.unshift(newEle);
+}
   
