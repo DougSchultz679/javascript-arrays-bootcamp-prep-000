5dc5be4cd0499818a5167fa467f90d2aee1757cd
Douglas W Schultz <vxpx17@gmail.com>
2017-12-30 02:21:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;}
 
 function removeElementFromEndOfArray(array){
-  return array.slice(array.length-1);}
+  return array.slice((array.length)-1);}
   
   
